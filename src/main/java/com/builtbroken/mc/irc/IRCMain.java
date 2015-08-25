a71731016c25b302f5a065261443b3fd6fa4971b
Kolatra <t.louks88@gmail.com>
2015-08-25 05:29:05 -0600

    Initial mod commit.

---
+++ src/main/java/com/builtbroken/mc/irc/IRCMain.java
@@ -0,0 +1,11 @@
+package com.builtbroken.mc.irc;
+
+import com.builtbroken.mc.irc.util.Reference;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class IRCMain
+{
+
+}
---
+++ src/main/java/com/builtbroken/mc/irc/util/Reference.java
@@ -0,0 +1,8 @@
+package com.builtbroken.mc.irc.util;
+
+public class Reference
+{
+    public static final String MODID = "veirc";
+    public static final String NAME = "VEIRC";
+    public static final String VERSION = "@VERSION@";
+}
